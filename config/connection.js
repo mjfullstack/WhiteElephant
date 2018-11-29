f769bd515fad91e5cd6f45544fbd02cb38de9330
Shaun Ericson <shaun.ericson@outlook.com>
2018-11-28 20:58:16 -0800

    DB Schema and DB Connection

---
+++ config/connection.js
@@ -0,0 +1,16 @@
+// Connection to DB
+
+var Sequelize = require("sequelize");
+
+var sequelize = new Sequelize("white_elephant_db", "root", "IHAZUnicorn", {
+  host: "localhost",
+  port: 3306,
+  dialect: "mysql",
+  pool: {
+    max: 5,
+    min: 0,
+    idle: 10000
+  }
+});
+
+module.exports = sequelize;
