d904686289baa8b2e1b7de750648561602c199ee
Shaun Ericson <shaun.ericson@outlook.com>
2018-11-29 20:23:41 -0800

    added tweaks to sequelize model

--- db/schema.sql
+++ db/schema.sql
@@ -1,12 +1,12 @@
 -- DB SCHMEA - Do Not Use - Created w/ Sequalize --
 
-drop database if exists game_details_db;
+drop database if exists white_elephant_db;
 
-create database game_details_db;
+create database white_elephant_db;
 
 use white_elephant_db;
 
-create table white_elephant_db (
+create table game_details (
     id int not null auto_increment,
     game_name varchar(100) not null,
     gift_dollar_max int not null,
